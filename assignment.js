48ddf908cba521eaead6c9341aa61f59e69043c2
Mohammed Imdad Ulla Khan <68274252+MohammedImdadUllaKhan@users.noreply.github.com>
2020-07-16 21:19:18 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,22 @@
+// creating my list
+let my_list=["apple","banana","orange","strawberry","passion fruit"];
+// addin the value in the my_list
+my_list.push("grapes");
+// removes the value fro the list from the end
+my_list.pop();
+console.table(my_list)
+my_list.pop();
+console.table(my_list)
+console.log(my_list[2])
+// outputs the length of the list
+console.log(my_list.length)
+// text
+var text="my name is shafia iam 12 yers old kid i know how to code in visual code"
+console.log(text)
+var aaa=text.split(" ")
+console.table(aaa)
+console.log(aaa.slice(1,10,2))
+var bbb=aaa.reverse()
+console.log(bbb)
+bbb.sort()
+console.log
